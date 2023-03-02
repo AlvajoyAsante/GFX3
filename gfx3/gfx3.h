66f5a784b9e6359732a23d4b5cf5d4d3b7f69e62
Alvajoy Asante <33734347+Overload02@users.noreply.github.com>
2023-03-02 11:22:12 -0500

    ADDED LICENSE

--- gfx3/gfx3.h
+++ gfx3/gfx3.h
@@ -1,3 +1,26 @@
+/**
+ * @file gfx3.h
+ * @brief This file contains the sprite stacking library
+ * 
+ * @author Alvajoy "Alvajoy123" Asante
+ * @version 1.0
+ * @copyright Copyright (c) 2018 - 2022
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef GFX3_H
 #define GFX3_H
 
