9e2a33486b9f05c268b200268655cf08f6735503
Alvajoy Asante <33734347+Overload02@users.noreply.github.com>
2023-03-04 14:24:18 -0500

    updated copyright year

--- test/src/gfx3/gfx3.h
+++ test/src/gfx3/gfx3.h
@@ -4,7 +4,7 @@
  *
  * @author Alvajoy "Alvajoy123" Asante
  * @version 1.0
- * @copyright Copyright (c) 2018 - 2022
+ * @copyright Copyright (c) 2018 - 2023
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
